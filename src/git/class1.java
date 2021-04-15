0e650da053ef136a19214a946c82252197c44f0c
Admin <Admin@192.168.0.8>
2021-04-15 22:45:32 +0530

    sanket pro1

--- src/git/class1.java
+++ src/git/class1.java
@@ -5,6 +5,13 @@ public class class1 {
 	public static void main(String[] args) {
 		System.out.println("hiiii");
 		
+		
+		System.out.println("my name is sanket");
+		
+		System.out.println("my favourite movie");
+		
+		System.out.println("testing");
+		
 	}
 
 }
